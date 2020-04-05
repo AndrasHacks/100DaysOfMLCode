12c63094f1213b3e417ee7705e2f70d79ef1afba
AndrasHacks <mr.acs.andras@gmail.com>
2020-04-05 22:10:14 +0200

    Greedy Best-First Search and A* searches.

--- cs50ai/week1/my_maze_implementation.py
+++ cs50ai/week1/my_maze_implementation.py
@@ -36,10 +36,85 @@ class QueueFrontier (Frontier):
             self.explored.append(node.state)
 
 
+# Now in this maze problem will have the heuristic as
+# "Get the lowest Manhattan Distance item from the frontier".
+class GreedyBestFirstSearchFrontier():
+
+    def __init__(self, manhattan_distances):
+        self.frontier = []
+        self.explored = []
+        self.manhattan_distances = manhattan_distances
+
+    def add(self, node):
+        if node.state not in self.explored:
+            self.frontier.append(node)
+            self.explored.append(node.state)
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        # get the min manhattan distance element
+        min_index = 0
+        first_x, first_y = self.frontier[0].state
+        min_manhattan_dist = self.manhattan_distances[first_x][first_y]
+        for i in range(len(self.frontier)):
+            x, y = self.frontier[i].state
+            current_manhattan_distance = self.manhattan_distances[x][y]
+            if current_manhattan_distance < min_manhattan_dist:
+                min_index = i
+                min_manhattan_dist = current_manhattan_distance
+        nodeToReturn = self.frontier[min_index]
+        del self.frontier[min_index]
+        return nodeToReturn
+
+
+# A* search gets the node with the lowest h(n) + g(n) value
+# This means, that it does not only consider the heuristic, but also
+# the cost of getting to the node.
+class AStarFrontier():
+    def __init__(self, manhattan_distances):
+        self.frontier = []
+        self.explored = []
+        self.manhattan_distances = manhattan_distances
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def add(self, node):
+        if node.state not in self.explored:
+            self.frontier.append(node)
+            self.explored.append(node.state)
+
+    def remove(self):
+        min_index = 0
+        first_x, first_y = self.frontier[0].state
+
+        # We also consider here the cost of getting to the element, not only
+        # the Manahattan Distance heuristics.
+        min_heuristic_and_cost_value = self.manhattan_distances[first_x][first_y] + \
+            self.frontier[0].path_cost
+        for i in range(len(self.frontier)):
+            x, y = self.frontier[i].state
+            curr_hc_value = self.manhattan_distances[x][y] + \
+                self.frontier[i].path_cost
+            if curr_hc_value < min_heuristic_and_cost_value:
+                min_index = i
+                min_heuristic_and_cost_value = curr_hc_value
+        nodeToReturn = self.frontier[min_index]
+        del self.frontier[min_index]
+        return nodeToReturn
+
+
 class Maze:
 
     walls = []
     solution = []
+
+    # Taxicab geometry:
+    # https://xlinux.nist.gov/dads/HTML/manhattanDistance.html
+    # |x1 - x2| + |y1 - y2|
+    manhattan_distances = []
 
     def __init__(self, mazeContent):
         height = len(mazeContent)
@@ -59,6 +134,15 @@ class Maze:
                 else:
                     row.append(True)
             self.walls.append(row)
+
+        # get all Manhattan distances
+        goalx, goaly = self.goal
+        for i in range(height):
+            row = []
+            for j in range(width):
+                manhattan_dist = abs(goalx - i) + abs(goaly - j)
+                row.append(manhattan_dist)
+            self.manhattan_distances.append(row)
 
     def draw(self):
         height = len(self.walls)
@@ -82,8 +166,10 @@ class Maze:
 
     def solve(self):
         count_states_exoplored = 0
-        frontier = StackFrontier()
+        # frontier = StackFrontier()
         # frontier = QueueFrontier()
+        # frontier = GreedyBestFirstSearchFrontier(self.manhattan_distances)
+        frontier = AStarFrontier(self.manhattan_distances)
         frontier.add(Node(self.start, None, None, 0))
 
         while True:
@@ -120,11 +206,16 @@ class Maze:
         return result
 
 
-fileName = './maze3.txt'
-with open(fileName) as file:
-    content = file.readlines()
-maze = Maze(content)
-print(maze.draw())
-steps_taken = maze.solve()
-print('Solution found in %d steps' % steps_taken)
-print(maze.draw())
+def main():
+    fileName = './maze2.txt'
+    with open(fileName) as file:
+        content = file.readlines()
+    maze = Maze(content)
+    print(maze.draw())
+    steps_taken = maze.solve()
+    print('Solution found in %d steps' % steps_taken)
+    print(maze.draw())
+
+
+if __name__ == "__main__":
+    main()
