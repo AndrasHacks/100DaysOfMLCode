3d1e9a610a7b73acfd87f2b4ceffa6d66f4a58bd
AndrasHacks <mr.acs.andras@gmail.com>
2020-04-20 22:21:10 +0200

    Minmax implemented without pruning

--- cs50ai/week1/tic-tac-toe/tictactoe.py
+++ cs50ai/week1/tic-tac-toe/tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+from copy import copy, deepcopy
 
 X = "X"
 O = "O"
@@ -22,50 +23,72 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    if terminal(board):
+        return None
+
+    if board == initial_state():
+        return X
+
+    x_count = 0
+    o_count = 0
+    for row in board:
+        for place in row:
+            if place == X:
+                x_count += 1
+            elif place == O:
+                o_count += 1
+
+    if x_count > o_count:
+        return O
+    elif x_count < o_count:
+        return X
+    return X
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    if terminal(board):
+        return None
+    result = []
+    for i, row in enumerate(board):
+        for j, element in enumerate(row):
+            if element == None:
+                result.append((i, j))
+    return set(result)
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    new_board = deepcopy(board)
+    new_board[action[0]][action[1]] = player(board)
+    return new_board
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    win_result = utility(board)
+    if win_result == 1:
+        return X
+    elif win_result == -1:
+        return O
+    return None
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    # horizontal check
-    if check_board_for_win(board):
+    win_result = utility(board)
+    if win_result == 1 or win_result == -1:
         return True
 
-    # vertical check
-    if check_board_for_win(zip(*board)):
-        return True
-
-    # left horizontal check:
-    if check_row_for_win(list(zip(*create_diagonal_matrix(board, "left")))[0]):
-        return True
-
-    # right horizontal check:
-    if check_row_for_win(list(zip(*create_diagonal_matrix(board, "right")))[-1]):
-        return True
-
+    # Check for all places filled:
     if None not in set([item for sublist in board for item in sublist]):
         return True
 
@@ -91,24 +114,69 @@ def create_diagonal_matrix(matrix, side='left'):
 
 def check_board_for_win(board):
     for row in board:
-        if check_row_for_win(row):
-            return True
-    return False
+        row_win_result = check_row_for_win(row)
+        if row_win_result[0]:
+            return row_win_result
+    return (False, None)
 
 
 def check_row_for_win(row):
-    return len(set(row)) == 1 and row[0] != EMPTY
+    return (len(set(row)) == 1 and row[0] != EMPTY, row[0])
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    winner = None
+    horizontal_win_result = check_board_for_win(board)
+    vertical_win_result = check_board_for_win(zip(*board))
+    left_diagonal_as_row = list(zip(*create_diagonal_matrix(board, "left")))[0]
+    right_diagonal_as_row = list(
+        zip(*create_diagonal_matrix(board, "right")))[-1]
+
+    left_diagonal_win_result = check_row_for_win(left_diagonal_as_row)
+    right_diagonal_win_result = check_row_for_win(right_diagonal_as_row)
+
+    if horizontal_win_result[0]:
+        winner = horizontal_win_result[1]
+    elif vertical_win_result[0]:
+        winner = vertical_win_result[1]
+    elif left_diagonal_win_result[0]:
+        winner = left_diagonal_win_result[1]
+    elif right_diagonal_win_result[0]:
+        winner = right_diagonal_win_result[1]
+
+    if winner == 'X':
+        return 1
+    elif winner == 'O':
+        return -1
+    return 0
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    if terminal(board):
+        return None
+
+    return minimax_impl(board, None)[0]
+
+
+def minimax_impl(board, action):
+    if terminal(board):
+        return (action, utility(board))
+
+    current_player = player(board)
+    next_actions = actions(board)
+    minimax = (None, -2 if player == X else 2)
+    for act in next_actions:
+        _, value = minimax_impl(result(board, act), act)
+        if player == 'X':
+            if value > minimax[1]:
+                minimax = (act, value)
+        else:
+            if value < minimax[1]:
+                minimax = (act, value)
+    return minimax
--- cs50ai/week1/tic-tac-toe/tictactoe.tests.py
+++ cs50ai/week1/tic-tac-toe/tictactoe.tests.py
@@ -2,6 +2,46 @@ import unittest
 
 ttt = __import__('tictactoe')
 
+horizonal_wins = [
+    [
+        ['X', 'X', 'X'],
+        [None, None, None],
+        [None, None, None]
+    ],
+    [
+        [None, None, None],
+        ['X', 'X', 'X'],
+        [None, None, None]
+    ],
+    [
+        [None, None, None],
+        [None, None, None],
+        ['X', 'X', 'X']
+    ]
+]
+
+vertical_wins = [
+    [
+        ['X', None, None],
+        ['X', None, None],
+        ['X', None, None]
+    ],
+    [
+        [None, 'X', None],
+        [None, 'X', None],
+        [None, 'X', None]
+    ],
+    [
+        [None, None, 'X'],
+        [None, None, 'X'],
+        [None, None, 'X']
+    ]
+]
+
+draw_board = [['O', 'X', 'O'],
+              ['O', 'X', 'X'],
+              ['X', 'O', 'X']]
+
 
 class TicTacToeTests(unittest.TestCase):
 
@@ -11,50 +51,14 @@ class TicTacToeTests(unittest.TestCase):
         self.assertEqual(False, result)
 
     def test_X_in_horizontal_lines_returns_true(self):
-        board1 = [
-            ['X', 'X', 'X'],
-            [None, None, None],
-            [None, None, None]
-        ]
-        board2 = [
-            [None, None, None],
-            ['X', 'X', 'X'],
-            [None, None, None]
-        ]
-        board3 = [
-            [None, None, None],
-            [None, None, None],
-            ['X', 'X', 'X']
-        ]
-        result1 = ttt.terminal(board1)
-        result2 = ttt.terminal(board2)
-        result3 = ttt.terminal(board3)
-        self.assertEqual(True, result1)
-        self.assertEqual(True, result2)
-        self.assertEqual(True, result3)
+        for board in horizonal_wins:
+            result = ttt.terminal(board)
+            self.assertEqual(True, result)
 
     def test_X_in_vertical_lines_returns_true(self):
-        board1 = [
-            ['X', None, None],
-            ['X', None, None],
-            ['X', None, None]
-        ]
-        board2 = [
-            [None, 'X', None],
-            [None, 'X', None],
-            [None, 'X', None]
-        ]
-        board3 = [
-            [None, None, 'X'],
-            [None, None, 'X'],
-            [None, None, 'X']
-        ]
-        result1 = ttt.terminal(board1)
-        result2 = ttt.terminal(board2)
-        result3 = ttt.terminal(board3)
-        self.assertEqual(True, result1)
-        self.assertEqual(True, result2)
-        self.assertEqual(True, result3)
+        for board in vertical_wins:
+            result = ttt.terminal(board)
+            self.assertEqual(True, result)
 
     def test_X_in_diagonal_returns_true(self):
         board = [
@@ -92,13 +96,91 @@ class TicTacToeTests(unittest.TestCase):
         ])
 
     def test_board_is_draw_returns_true(self):
-        board = [
-            ['O', 'X', 'O'],
-            ['O', 'X', 'X'],
-            ['X', 'O', 'X']
-        ]
-        result = ttt.terminal(board)
+        result = ttt.terminal(draw_board)
         self.assertEqual(result, True)
+
+    def test_player_initial_state_returns_X(self):
+        self.assertEqual('X', ttt.player(ttt.initial_state()))
+
+    def test_player_same_amout_of_X_and_O_returns_X(self):
+        board = [
+            ['X', None, None],
+            ['O', None, None],
+            [None, None, None]
+        ]
+        self.assertEqual('X', ttt.player(board))
+
+    def test_more_X_than_O_returns_O(self):
+        board = [
+            ['X', None, None],
+            ['O', None, None],
+            [None, 'X', None]
+        ]
+        self.assertEqual('O', ttt.player(board))
+
+    def test_more_O_than_X(self):
+        board = [
+            ['X', None, None],
+            ['O', None, None],
+            [None, 'O', None]
+        ]
+        self.assertEqual('X', ttt.player(board))
+
+    def test_player_gets_terminal_state_returns_none(self):
+        self.assertIsNone(ttt.player(draw_board))
+
+    def test_actions_terminal_board_retruns_none(self):
+        self.assertIsNone(ttt.actions(draw_board))
+
+    def test_actions_initial_state_returns_9_actions(self):
+        expected_result = set([(x, y) for x in range(0, 3)
+                               for y in range(0, 3)])
+        self.assertEqual(expected_result, ttt.actions(ttt.initial_state()))
+
+    def test_actions_two_places_returns_indexes(self):
+        board = [
+            ['X', None, 'X'],
+            ['O', 'O', 'X'],
+            ['O', 'X', None]
+        ]
+        self.assertEqual(set([(0, 1), (2, 2)]), ttt.actions(board))
+
+    def test_result_X_places_a_move_returns_correct_board(self):
+        board = [
+            ['X', None, None],
+            ['O', None, None],
+            [None, None, None]
+        ]
+        expected_result = [
+            ['X', 'X', None],
+            ['O', None, None],
+            [None, None, None]
+        ]
+        action = (0, 1)
+        self.assertEqual(expected_result, ttt.result(board, action))
+
+    def test_result_O_places_a_move_returns_correct_board(self):
+        board = [
+            ['X', 'X', None],
+            ['O', None, None],
+            [None, None, None]
+        ]
+        expected_result = [
+            ['X', 'X', None],
+            ['O', None, None],
+            ['O', None, None]
+        ]
+        action = (2, 0)
+        self.assertEqual(expected_result, ttt.result(board, action))
+        self.assertEqual([
+            ['X', 'X', None],
+            ['O', None, None],
+            [None, None, None]
+        ], board)
+
+    def test_minimax_initial_state_X_can_move(self):
+        result = ttt.minimax(ttt.initial_state())
+        self.assertEqual((0, 1), result)
 
 
 if __name__ == '__main__':
